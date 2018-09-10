6e6d0d3564c2ad843fdedc3ea0fb4d37750e6c27
Steven Brooks <srbrooks@miners.utep.edu>
2018-09-10 11:58:38 -0600

    Implemented basic file input/output redirection.

--- shell/shell.py
+++ shell/shell.py
@@ -4,14 +4,43 @@ import os, sys, re
 
 pid = os.getpid() # store shell's pid
 
+'''
+Function that handles redirection arguments in an array of input arguments.
+param args : A list of string arguments read from the shell.
+'''
+def redirect(args):
+    if ">" in args:
+        os.close(1) # close standard out
+        filename = args[args.index(">") + 1] # get filename
+
+        sys.stdout = open(filename, "w+") # set standard output
+        os.set_inheritable(sys.stdout.fileno(), True)
+
+        args.pop(args.index(">") + 1) # remove redirection
+        args.pop(args.index(">"))
+
+    if "<" in args:
+        os.close(0) # close standard in
+        filename = args[args.index("<") + 1] # get filename
+
+        sys.stdin = open(filename, "r") # set standard input
+        os.set_inheritable(sys.stdin.fileno(), True)
+
+        args.pop(args.index("<") + 1) # remove redirection
+        args.pop(args.index("<"))
+
+
+### main
 while True:
-    args = str(input("%s$" % (os.getcwd()))).split() # get user input and split on spaces
+    args = str(input("%s$ " % (os.getcwd()))).split() # get user input and split on spaces
 
     fork_code = os.fork()
 
     if fork_code == 0: # in child
         directories = re.split(":", os.environ['PATH'])
         directories.append(os.getcwd())
+
+        redirect(args) # handle any redirections
 
         for dir in directories: # try each directory in PATH and current working directory
             program = "%s/%s" % (dir, args[0])
